74419d96c3cc01abd9a68cd63f9c54f629ac4a6a
Alexandr Gajdosh <alexandr@agm.com>
2018-10-22 16:40:18 +0300

    devise confirmation disale login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,5 +14,4 @@ Devise.setup do |config|
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
-  config.allow_unconfirmed_access_for = nil
 end
