e3def0b117a762dfffb939013aa5755c40270585
Alexandr Gajdosh <alexandr@agm.com>
2018-10-24 23:10:08 +0300

    email previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.asset_host = 'http://localhost:3000'
+
+  config.action_mailer.preview_path = Rails.root.join('spec', 'mailers', 'previews')
 end
