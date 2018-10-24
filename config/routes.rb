a4204a2b5878e385ddf26b2ca15447d4985b9b64
Alexandr Gajdosh <alexandr@agm.com>
2018-10-24 17:06:12 +0300

    rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  constraints(AdminRouteConstraint) do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
+
   # devise_for :users
   devise_for :users, controllers: {
     registrations: 'users/registrations',
