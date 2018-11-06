b1411c6cc1b821729f98fc74c5ca868999f194d4
Dmitriy Bielorusov <d.belorusov@gmail.com>
2018-11-06 12:51:32 +0200

    Make Helbers for state classes of blocks in dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,6 @@ module ApplicationHelper
     end
   end
 
-  def current_step
-    return 0 if @invest_set&.ready?
-    return 1 unless @source_account&.ready?
-    return 2 if @source_account&.ready? && !@invest_account&.ready?
-    return 3 if @source_account&.ready? && @invest_account&.ready?
-  end
-
   def errors_in_block_for(object)
     return unless object&.errors&.any?
 
@@ -29,11 +22,5 @@ module ApplicationHelper
       message += "<li>#{m}</li>"
     end
     "#{message}</div>"
-  end
-
-  def transaction_class_by_status(status)
-    return '' if status == 'cancelled'
-
-    "status-#{status}"
   end
 end
